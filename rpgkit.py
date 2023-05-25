07c5aa7241f8f04769df08ff9cf36dce0e7cc5c6
Jonathan Landry <mouseroot@gmail.com>
2023-05-25 14:36:50 -0500

    Initial Upload

---
+++ rpgkit.py
@@ -0,0 +1,17 @@
+
+
+class character:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+class item:
+    def __init__(self, name, value=1,count=1):
+        self.name = name
+        self.value = value
+        self.count = count
+        self.flags = []
+
+    def use_item(self, target):
+        self.count -= 1
+        
