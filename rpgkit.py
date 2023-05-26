c5acf82e6150945c18128c6abc8b81ea81d93b7a
Jonathan Landry <mouseroot@gmail.com>
2023-05-26 02:55:09 -0500

    added weapon class, fixed npc class

--- rpgkit.py
+++ rpgkit.py
@@ -3,9 +3,13 @@ class world:
         pass
 
 class character:
-    def __init__(self, name, health):
+    def __init__(self, name, health=100):
         self.name = name
         self.health = health
+        self.flags = []
+
+    def take_damage(self, amt):
+        self.health -= amt
 
 class item:
     def __init__(self, name, value=1,count=1):
@@ -21,3 +25,14 @@ class npc(character):
     def __init__(self, name):
         character.__init__(self, name)
         self.dialog = []
+
+class weapon(item):
+    def __init__(self, name, base_damage=1):
+        item.__init__(self,name)
+        self.base_damage = base_damage
+
+    def use_item(self, target):
+        target.take_damage(self.base_damage)
+        super().use_item(target)
+
+    
