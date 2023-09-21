df25413ca9baa5e533ff747d3668b2281a5a70a3
Mouseroot <mouseroot@gmail.com>
2023-09-21 06:52:42 -0500

    Update __init__.py
    Removed old World refrence
    Added Skill and Spell

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .base import ResourceManager, MonsterManager, ItemManager, BlueprintManager, World
+from .base import ResourceManager, MonsterManager, ItemManager, BlueprintManager, Skill, Spell
 from .entity import InventorySlot, Inventory, Entity, Character, Enemy
 from .item import Item, HealingItem, Weapon, Armor, RangedWeapon
 from .crafting import Blueprint
