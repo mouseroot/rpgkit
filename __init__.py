677c0a6e8728a5942b35f4f007c266cfe670bb84
Mouseroot <mouseroot@gmail.com>
2023-09-20 22:09:51 -0500

    Update __init__.py
    Added RangedWeapon

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .base import ResourceManager, MonsterManager, ItemManager, BlueprintManager, World
 from .entity import Entity, Character, Enemy
-from .item import Item, HealingItem, Weapon, Armor
+from .item import Item, HealingItem, Weapon, Armor, RangedWeapon
