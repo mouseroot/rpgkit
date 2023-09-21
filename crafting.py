1e39ca6f833d76f80aa4a51d86ac05fed8a89352
Mouseroot <mouseroot@gmail.com>
2023-09-21 05:02:43 -0500

    Update crafting.py
    Updated Blueprint class
    added id/name
    fixed can_craft method
    fixed craft method

--- crafting.py
+++ crafting.py
@@ -1,12 +1,31 @@
 
 class Blueprint:
-    def __init__(self, req_items, result_item):
+    def __init__(self, name, req_items, result_item):
+        self.id = 0
+        self.name = name
         self.req_items = req_items
         self.result_item = result_item
 
     def can_craft(self, player):
-        return False
+        for item in self.req_items:
+            item_count = int(self.req_items[item])         
+            item_slot = player.inventory.has_item(item)
+            if item_slot:
+                count = player.inventory.get_count(item)
+                if count >= item_count:
+                    continue
+                else:
+                    return False
+            else:
+                return False
+        return True
+
 
     def craft(self, player):
-        print(f"Attempting to craft {self.result_item.name}")
+        if self.can_craft(player):
+            for item in self.req_items:
+                player.inventory.remove_item(item, int(self.req_items[item]))
+            player.inventory.add_item(self.result_item)
+            
+
         
