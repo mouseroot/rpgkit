42c5d6f2703c5bc3e74ba3add5ee6d9d89835ef4
Mouseroot <mouseroot@gmail.com>
2023-09-21 07:01:36 -0500

    Update __init__.py
    Changed import syntax

--- __init__.py
+++ __init__.py
@@ -1,4 +1,21 @@
-from .base import ResourceManager, MonsterManager, ItemManager, BlueprintManager, Skill, Spell
-from .entity import InventorySlot, Inventory, Entity, Character, Enemy
-from .item import Item, HealingItem, Weapon, Armor, RangedWeapon
+from .base import (
+    ResourceManager, 
+    MonsterManager, 
+    ItemManager, 
+    BlueprintManager, Skill, Spell
+)
+from .entity import (
+    InventorySlot, 
+    Inventory, 
+    Entity, 
+    Character, 
+    Enemy
+)
+from .item import (
+    Item, 
+    HealingItem, 
+    Weapon, 
+    Armor, 
+    RangedWeapon
+)
 from .crafting import Blueprint
